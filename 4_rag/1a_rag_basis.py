9b1ff5877223bbcf2f2f5661aec4ab6848fd7468
Mukhammed <zhumalimuhammed@gmail.com>
2025-10-28 15:13:02 +0500

    load env

--- 4_rag/1a_rag_basis.py
+++ 4_rag/1a_rag_basis.py
@@ -1,9 +1,12 @@
 import os
 
+from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+
+load_dotenv() 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "books", "sample.txt")
